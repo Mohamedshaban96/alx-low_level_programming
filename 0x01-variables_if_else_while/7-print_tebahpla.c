940cacc951e58e612bbd909369a84cef70a88c87
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-08-11 19:30:16 -0700

    t e

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * if - to check something is true or fault and do action according to that
+ *
+ * Descreption: A program to diff between negative, positive or zero numbers
+ *
+ * Return: Always 0 Sucess
+*/
+int main(void)
+{
+	int y = 122;
+
+	while (y > 96)
+{
+	putchar (y);
+	y = y - 1;
+}
+	putchar ('\n');
+
+
+	return (0);
+}

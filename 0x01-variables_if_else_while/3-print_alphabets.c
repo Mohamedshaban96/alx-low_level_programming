2760e2f6aff10c6e3fd0baec355da884bb38f5d3
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-08-11 18:53:43 -0700

    task three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * if - to check something is true or fault and do action according to that
+ *
+ * Descreption: A program to diff between negative, positive or zero numbers
+ *
+ * Return: Always 0 Sucess
+*/
+int main(void)
+{
+	int y = 97;
+
+	while (y < 123)
+{
+	putchar (y);
+	y = y + 1;
+}
+	y = 65;
+
+	while (y < 91)
+{
+	putchar (y);
+	y = y + 1;
+}
+	putchar ('\n');
+
+	return (0);
+}

e01245aeba16a3a67128fcb67cd308c0fd572fef
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-08-11 19:11:33 -0700

    t s

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * if - to check something is true or fault and do action according to that
+ *
+ * Descreption: A program to diff between negative, positive or zero numbers
+ *
+ * Return: Always 0 Sucess
+*/
+int main(void)
+{
+	int x = 0;
+
+
+	while (x < 10)
+	printf("%d \n", x);
+	x = x + 1;
+
+	return (0);
+}

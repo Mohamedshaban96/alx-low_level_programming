f2e2d9932277d2bc8d1fb0541f897d198bcfe9b0
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-08-11 17:21:22 -0700

    second task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * if - to check something is true or fault and do action according to that
+ *
+ * Descreption: A program to diff between negative, positive or zero numbers
+ *
+ * Return: Always 0 Sucess
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int l = n % 10
+
+	if (l > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5", n, l);
+	}
+	else if (l == 0)
+	{
+	printf("Last digit of %d is %d and is 0", n, l);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0", n, l);
+	}
+	return (0);
+}

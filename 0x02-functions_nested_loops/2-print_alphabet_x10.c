684c9bff25cffb4306789d3cf0452ae6fe03b6e4
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-08-15 17:34:10 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alph ten times
+ *
+ * Return: Always 0 Success
+ */
+
+void print_alphabet_x10(void)
+{
+	int p = 0, y = 97;
+
+	while (p < 10)
+	{
+		while (y < 123)
+			_putchar(y);
+		y++;
+	}
+	_putchar('\n');
+
+		p++;
+}

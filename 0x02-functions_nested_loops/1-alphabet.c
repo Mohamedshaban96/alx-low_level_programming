2e2cb26e0ac00aef85526d159ac40d973a44fa70
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-08-15 17:20:23 -0700

    t2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int y;
+
+	y = 97;
+
+	while (y < 123)
+{
+	_putchar(y);
+
+	y++;
+}
+	_putchar('\n');
+}

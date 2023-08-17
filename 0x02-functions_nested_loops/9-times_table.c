8d7a53ad21ac276c76a77b8aeaa97358fa483f85
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-08-16 19:06:43 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - print the numbers tabel.
+ *
+ * Return: Always 0.
+ */
+
+
+void times_table(void)
+{
+	int n, x, y;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+
+		for (x = 1; x <= 9; x++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			y = n * x;
+		if (y <= 9)
+			_putchar(' ');
+		else
+			_putchar((y / 10) + '0');
+
+
+		_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

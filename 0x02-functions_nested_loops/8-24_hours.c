01c8c28780823b34bbce4c9fbc2c6cc9ae1ee9f7
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-08-16 17:34:18 -0700

    test

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int a = 0, b, c, d;
+
+	while (a < 10)
+{
+	b = 0;
+	while (b < 10)
+	{
+	while (c < 10)
+	{
+		while (d < 10)
+		{
+			_putchar('a');
+			d++;
+		}
+		c++;
+	}
+	b++;
+	}
+	a++;
+}
+}

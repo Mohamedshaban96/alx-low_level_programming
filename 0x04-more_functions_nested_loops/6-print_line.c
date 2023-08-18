0e1b9afd97b6c19435ba88a75d11a6c1f5447803
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-08-17 18:59:50 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - print a line
+ *
+ * @n:the number of times the character _ should be printed
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int y;
+
+	if (n < 1)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		y = 0;
+		while (y <= n)
+		{
+			_putchar('_');
+		y++;
+		}
+		_putchar('\n');
+	}
+}

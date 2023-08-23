609e6b7658a6470bf8e2bcab9696d93026fef655
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-08-22 18:40:15 -0700

    task 6 hf

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,15 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j += 2)
+		_putchar(str[j]);
 
 	_putchar('\n');
 }

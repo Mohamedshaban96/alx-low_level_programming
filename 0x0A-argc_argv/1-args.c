78984fb6c7725dc73cc0ad174959cec2b2fc4591
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-09-02 02:08:39 +0000

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
 * @argv: pointer to an array of arguments
 * Return: 0 on success
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
 {

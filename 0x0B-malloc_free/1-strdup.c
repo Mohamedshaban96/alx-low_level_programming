7fb540f2c4dda3b3ddafd6a3ca13760f246b157b
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-09-05 14:26:11 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - duplicate a string in a reserved memory in heap
+ *
+ * @str: a pointer to the string that will be copied
+ * Return: NULL in case of errors, a pointer to dup string if success
+ */
+
+char *_strdup(char *str)
+{
+	char *ns;
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	if (str == NULL)
+		return (NULL);
+
+	i++;
+	ns = malloc(sizeof(char) * i);
+
+	if (ns == NULL)
+		return (NULL);
+
+	while (j < i)
+	{
+		ns[j] = str[j];
+		j++;
+	}
+	return (ns);
+}
+

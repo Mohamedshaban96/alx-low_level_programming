8c3767101ff16f6418169d5c8fcd9efd35674678
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-09-07 19:32:42 -0700

    task3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *array_range - creates an array of integers from given start point
+ * @min: starting int
+ * @max: end inr
+ *
+ * Return: pointer to the newly created array or NULL if fail
+ */
+
+int *array_range(int min, int max)
+{
+	int i, j;
+	int *ar;
+
+	if (min > max)
+		return (NULL);
+
+	i = max - min + 1;
+
+	ar = malloc(sizeof(int) * i);
+
+	if (ar == NULL)
+		return (NULL);
+
+	j = 0;
+	while (j < i)
+	{
+		ar[j] = min++;
+		i++;
+	}
+	return (ar);
+}

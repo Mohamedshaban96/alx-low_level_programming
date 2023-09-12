3261aef26d1ec695272d41142e50226d20ded7fb
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-09-11 19:42:16 -0700

    t0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creat structure for dog
+ * @name: its name
+ * @age: its age
+ * @owner: its owner name;
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char owner;
+};
+#endif

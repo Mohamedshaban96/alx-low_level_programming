f2be6b96fb6eeca940854dad024a4e1039e0b6a1
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-09-11 19:59:30 -0700

    all

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,16 +10,16 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, len_Name, len_Owner;
+	int i, lenN, lenO;
 
 	struct dog *n_dog = NULL;
 
 	lenN = 0;
 	while (name[lenN] != '\0')
 		lenN++;
-	len_Owner = 0;
+	lenO = 0;
 	while (owner[lenO] != '\0')
-		len_Owner++;
+		lenO++;
 
 	n_dog = malloc(sizeof(struct dog));
 	if (n_dog == NULL)
@@ -27,14 +27,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(n_dog);
 		return (NULL);
 	}
-	n_dog->name = malloc(len_Name + 1);
+	n_dog->name = malloc(lenN + 1);
 	if (n_dog->name == NULL)
 	{
 		free(n_dog->name);
 		free(n_dog);
 		return (NULL);
 	}
-	n_dog->owner = malloc(len_Owner + 1);
+	n_dog->owner = malloc(lenO + 1);
 	if (n_dog->owner == NULL)
 	{
 		free(n_dog->name);
@@ -42,9 +42,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(n_dog);
 		return (NULL);
 	}
-	for (i = 0; i <= len_Name; i++)
+	for (i = 0; i <= lenN; i++)
 		n_dog->name[i] = name[i];
-	for (i = 0; i <= len_Owner; i++)
+	for (i = 0; i <= lenO; i++)
 		n_dog->owner[i] = owner[i];
 	n_dog->age = age;
 	return (n_dog);

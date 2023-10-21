854ac9cee4a37c6ba32b1a5110c137da34d6f229
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-10-20 17:47:13 -0700

    t2 bug fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
+		len = write(fd, text_content, strlen(text_content));
 	close(fd);
 	if (len == -1)
 		return (-1);

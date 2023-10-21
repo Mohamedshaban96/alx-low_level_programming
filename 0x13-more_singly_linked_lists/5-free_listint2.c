194652b687cc6c39e013ff39331c861e4e224297
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-10-20 18:39:59 -0700

    t5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a list_t list.
+ * @head: pointer to list structure
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+
